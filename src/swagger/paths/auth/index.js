1fbb624f9fe6eb0a823bad3233017a3345350928
Yaser Sharifi <yassersharifi74@gmail.com>
2023-04-13 22:43:52 +0330

    fixed swagger docs of user find all

--- src/swagger/paths/auth/index.js
+++ src/swagger/paths/auth/index.js
@@ -6,8 +6,12 @@ const YAML = require('yaml');
 const loginAuth = fs.readFileSync(path.join(__dirname, 'login-auth.yaml'), 'utf8');
 const loginAuthParse = YAML.parse(loginAuth);
 
+const registerAuth = fs.readFileSync(path.join(__dirname, 'register-auth.yaml'), { encoding: 'utf8'});
+const registerAuthParse = YAML.parse(registerAuth);
+
 const authPaths = {
     ...loginAuthParse,
+    ...registerAuthParse,
 };
 
 module.exports = authPaths;
--- src/swagger/paths/user/index.js
+++ src/swagger/paths/user/index.js
@@ -6,8 +6,15 @@ const YAML = require('yaml');
 const createUser = fs.readFileSync(path.join(__dirname, 'create-user.yaml'), 'utf8');
 const createUserParse = YAML.parse(createUser);
 
+// find all user
+const findAllUser = fs.readFileSync(path.join(__dirname, 'find-all-user.yaml'), 'utf8');
+const findAllUserParse = YAML.parse(findAllUser);
+
 const userPaths = {
-    ...createUserParse,
+    '/users': {
+        ...findAllUserParse,
+        ...createUserParse,
+    }
 };
 
 module.exports = userPaths;
